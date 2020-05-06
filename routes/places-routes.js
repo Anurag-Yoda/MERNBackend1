0770fc81b488a64e60ba863fb329608cc81c8bee
Anurag-Yoda <anurag.404@outlook.com>
2020-05-06 20:19:42 +0530

    deployment stage

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,18 +1,44 @@
-const express = require("express");
-const placesController = require("../controllers/places-controller");
+const express = require('express');
+const { check } = require('express-validator');
+
+const placesControllers = require('../controllers/places-controllers');
+const fileUpload = require('../middleware/file-upload');
+const checkAuth = require('../middleware/check-auth');
+
 const router = express.Router();
-const {check} = require('express-validator');
- 
-router.get("/:pid", placesController.getPlaceById);
 
-router.get("/user/:uid", placesController.getPlacesByUserId);
+router.get('/:pid', placesControllers.getPlaceById);
 
-router.post("/",[check('title').not().isEmpty(), check('description').isLength({min:5 }), check('address').not().isEmpty()], placesController.createPlace);
+router.get('/user/:uid', placesControllers.getPlacesByUserId);
 
-router.patch("/:pid", [check('title').not().isEmpty(), check('description').isLength({min:5 })],placesController.updatePlace);
+router.use(checkAuth);
 
-router.delete("/:pid", placesController.deletePlace);
+router.post(
+  '/',
+  fileUpload.single('image'),
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 }),
+    check('address')
+      .not()
+      .isEmpty()
+  ],
+  placesControllers.createPlace
+);
 
+router.patch(
+  '/:pid',
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 })
+  ],
+  placesControllers.updatePlace
+);
 
+router.delete('/:pid', placesControllers.deletePlace);
 
 module.exports = router;
